a84bf27d22b46d4afd2f8f52dc65a087f4f96b9c
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-03-01 19:44:49 +0300

    new

--- hipo_projects/app/src/main/java/com/example/hipo_projects/model/Position.kt
+++
@@ -1,8 +0,0 @@
-package com.example.hipo_projects.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Position(
-    @SerializedName("id") val id: Int,
-    @SerializedName("name") val name: String
-)
