381d73f2f3b01a1ca9f509c5a1cf2222ffa985f2
GamzeSirakaya <sirakayagamze3@gmail.com>
2021-04-05 18:50:33 +0300

    a

--- Hipo_Intern/app/src/main/java/com/example/hipo_intern/model/Hipo.kt
+++
@@ -1,8 +0,0 @@
-import com.google.gson.annotations.SerializedName
-
-
-data class Hipo (
-
-	@SerializedName("position") val position : String,
-	@SerializedName("years_in_hipo") val years_in_hipo : Int
-)
--- Hipo_Intern/app/src/main/java/com/example/hipo_intern/model/Root.kt
+++
@@ -1,9 +0,0 @@
-import com.google.gson.annotations.SerializedName
-
-
-data class Root (
-
-	@SerializedName("company") val company : String,
-	@SerializedName("team") val team : String,
-	@SerializedName("members") val members : List<Member>
-)
